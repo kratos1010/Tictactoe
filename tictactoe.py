32571cc46c0548fc5afc481df9aace9e8bd1e254
ﾁｪﾝ ﾆｺﾗｽ <chen_nicholas@cyberagent.co.jp>
2015-11-28 02:16:10 +0900

    Rodger

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,15 @@
+from sys import stdout
 def main():
-    print("Hello world!")
+	theBoard = [["0", "1", "2"],
+				["3", "4", "5"],
+				["6", "7", "8"]]
 
+
+
+	for row in range(len(theBoard)):
+		print("%s|%s|%s") %(theBoard[row][0], theBoard[row][1], theBoard[row][2])
+		if row < len(theBoard) - 1:
+			print "-" * 5
+	
 if __name__ == "__main__":
-    main()
+	main()
